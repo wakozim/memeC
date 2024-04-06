e05ca08f637c62b653f24da7964e80564f8a8146
Lookins <lookins1q@gmail.com>
2024-04-06 23:35:13 +0500

    build: static raylib building

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 set -xe
 
 CFLAGS="-O3 -std=c99 -Wall -Wextra -pedantic -ggdb -I. `pkg-config --cflags raylib`"
-LIBS="-lm `pkg-config --libs raylib` -lglfw -ldl -lpthread"
+LIBS="-lm -lglfw -ldl -lpthread -lGL -lrt -lX11"
 
 mkdir -p ./build/
 
-clang $CFLAGS -o ./build/memeC ./src/main.c $LIBS
-clang $CFLAGS -o ./build/sequence ./src/sequence.c $LIBS
+clang $CFLAGS -o ./build/memeC ./src/main.c ./raylib/raylib-5.0_linux_amd64/lib/libraylib.a -no-pie -D_DEFAULT_SOURCE $LIBS 
+clang $CFLAGS -o ./build/sequence ./src/sequence.c ./raylib/raylib-5.0_linux_amd64/lib/libraylib.a -no-pie -D_DEFAULT_SOURCE $LIBS 
