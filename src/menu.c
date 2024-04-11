f22ddfeadad786c6e29ac14824edc6f76ad39caa
Lookins <lookins1q@gmail.com>
2024-04-11 19:16:40 +0500

    [menu.c] refactor: Removed separate text sizes for each menu item

--- src/menu.c
+++ src/menu.c
@@ -1,6 +1,10 @@
 #include "raylib.h"
 #include "screens.h"
 
+
+#define MENU_TEXT_SIZE 30
+
+
 void init_menu_game(void)
 {
     // Init menu variables here
@@ -13,20 +17,18 @@ GameScreen draw_menu_screen(void)
     int screen_height = GetScreenHeight();
 
     const char *sequence_text = "Press [S] to play the sequence";
-    int sequence_text_size = 25;
-    int sequence_text_width = MeasureText(sequence_text, sequence_text_size);
+    int sequence_text_width = MeasureText(sequence_text, MENU_TEXT_SIZE);
 
     const char *pairs_text = "Press [P] to play the pairs";
-    int pairs_text_size = 25;
-    int pairs_text_width = MeasureText(pairs_text, pairs_text_size);
+    int pairs_text_width = MeasureText(pairs_text, MENU_TEXT_SIZE);
 
     int pairs_text_x = screen_width/2 - pairs_text_width/2; 
-    int pairs_text_y = screen_height/2 - (pairs_text_size + sequence_text_size + 10)/2; 
-    DrawText(pairs_text, pairs_text_x, pairs_text_y, pairs_text_size, GREEN);
+    int pairs_text_y = screen_height/2 - (MENU_TEXT_SIZE * 2 + 10)/2; 
+    DrawText(pairs_text, pairs_text_x, pairs_text_y, MENU_TEXT_SIZE, GREEN);
 
     int sequence_text_x = screen_width/2 - sequence_text_width/2;               
-    int sequence_text_y = pairs_text_y + 10 + pairs_text_size; 
-    DrawText(sequence_text, sequence_text_x, sequence_text_y, sequence_text_size, GREEN);
+    int sequence_text_y = pairs_text_y + 10 + MENU_TEXT_SIZE; 
+    DrawText(sequence_text, sequence_text_x, sequence_text_y, MENU_TEXT_SIZE, GREEN);
     
     if (IsKeyPressed(KEY_S)) {
         return SEQUENCE;
