3f8bbe847220723fba31e116efac6ada8b67f37d
Lookins01 <lookins1q@gmail.com>
2024-09-22 15:02:40 +0500

    [style] fix the indentation and remove the trailing spaces

--- src/pairs.c
+++ src/pairs.c
@@ -15,7 +15,7 @@
 #define CELL_COLOR GRAY
 #define OPEN_CELL_COLOR BLACK
 
-typedef enum { 
+typedef enum {
     STATE_NONE,
     STATE_SHOW_FIELD,
     STATE_SHOW_PAIR,
@@ -47,30 +47,30 @@ int random_number(int min, int max)
     return (rand() % (max - min)) + min;
 }
 
-int get_rand_cell(void) 
+int get_rand_cell(void)
 {
     Cell *cell = NULL;
     int cell_index;
 
     do {
-        cell_index = random_number(0, LINES*COLUMNS); 
-        cell = &field[cell_index / COLUMNS][cell_index % COLUMNS]; 
+        cell_index = random_number(0, LINES*COLUMNS);
+        cell = &field[cell_index / COLUMNS][cell_index % COLUMNS];
     } while (cell->value != 0);
 
     return cell_index;
 }
 
 
-void get_rand_cells_pair(int *first, int *second) 
+void get_rand_cells_pair(int *first, int *second)
 {
     (*first) = get_rand_cell();
-     
+
     do {
         (*second) = get_rand_cell();
     } while (*first == *second);
 }
 
-void clear_field(void) 
+void clear_field(void)
 {
     for (int y = 0; y < LINES; y++) {
         for (int x = 0; x < COLUMNS; x++) {
@@ -90,11 +90,11 @@ void init_field(void)
         int value = random_number(0, (LINES*COLUMNS)/2);
         while (values[value]) {
             value = random_number(0, (LINES*COLUMNS)/2);
-        } 
+        }
         field[first_cell / COLUMNS][first_cell % COLUMNS].value = value + 1;
         field[second_cell / COLUMNS][second_cell % COLUMNS].value = value + 1;
         values[value] = true;
-    } 
+    }
 }
 
 void field_change_open_value(bool new_open_value)
@@ -116,7 +116,7 @@ void cell_event_handler(Cell *cell, bool is_cell_hovered)
             picked_cells.first = cell;
             picked_cells.first->open = true;
         } else if (picked_cells.first != cell) {
-             if (cell->value == picked_cells.first->value) {
+            if (cell->value == picked_cells.first->value) {
                 cell->open = true;
                 picked_cells.first->open = true;
                 picked_cells.first = NULL;
@@ -124,23 +124,23 @@ void cell_event_handler(Cell *cell, bool is_cell_hovered)
                 picked_cells.second = cell;
                 picked_cells.first->open = true;
                 picked_cells.second->open = true;
-                state = STATE_SHOW_PAIR; 
+                state = STATE_SHOW_PAIR;
             }
         }
     }
 }
 
 
-void init_pairs_game(void) 
+void init_pairs_game(void)
 {
     assert(LINES*COLUMNS % 2 == 0 && "Must be even");
-            
+
     srand(time(NULL));
 
     elapsed_time = 0.0f;
-    ttime = 0.0f; 
+    ttime = 0.0f;
     state = STATE_SHOW_FIELD;
-    
+
     picked_cells.first  = NULL;
     picked_cells.second = NULL;
 
@@ -151,64 +151,64 @@ void init_pairs_game(void)
 
 void draw_pairs_screen(void)
 {
-        if (IsKeyPressed(KEY_R)) {
-            picked_cells.first  = NULL;
-            picked_cells.second = NULL;
-            state = STATE_SHOW_FIELD;
+    if (IsKeyPressed(KEY_R)) {
+        picked_cells.first  = NULL;
+        picked_cells.second = NULL;
+        state = STATE_SHOW_FIELD;
+        ttime = 0.0f;
+        init_field();
+        field_change_open_value(true);
+    }
+
+    if (state == STATE_SHOW_FIELD) {
+        ttime += GetFrameTime();
+        if (ttime > 3.0f) {
+            state = STATE_USER_TURN;
+            field_change_open_value(false);
             ttime = 0.0f;
-            init_field();
-            field_change_open_value(true);
         }
+    } else if (state == STATE_SHOW_PAIR) {
+        ttime += GetFrameTime();
+        if (ttime > 0.5f) {
+            state = STATE_USER_TURN;
+            picked_cells.first->open = false;
+            picked_cells.second->open = false;
+            picked_cells.first = NULL;
+            picked_cells.second = NULL;
+            ttime = 0.0f;
+        }
+    }
 
-        if (state == STATE_SHOW_FIELD) {
-            ttime += GetFrameTime();
-            if (ttime > 3.0f) {
-                state = STATE_USER_TURN;
-                field_change_open_value(false);
-                ttime = 0.0f;
+    ClearBackground(BACKGROUND_COLOR);
+    int sx = GetScreenWidth()/2 - (COLUMNS*CELL_SIZE + (COLUMNS-1)*CELL_GAP)/2;
+    int sy = GetScreenHeight()/2 - (LINES*CELL_SIZE + (LINES-1)*CELL_GAP)/2;
+
+    for (int line = 0; line < LINES; line++) {
+        for (int column = 0; column < COLUMNS; column++) {
+            Cell *cell = &field[line][column];
+
+            int x = sx + (column*CELL_SIZE) + (column*CELL_GAP);
+            int y = sy + (line*CELL_SIZE) + (line*CELL_GAP);
+            Rectangle cell_rect = {x, y, CELL_SIZE, CELL_SIZE};
+            bool is_cell_hovered = CheckCollisionPointRec(GetMousePosition(), cell_rect);
+
+            Color color = cell->open ? OPEN_CELL_COLOR : CELL_COLOR;
+            DrawRectangle(x, y, CELL_SIZE, CELL_SIZE, color);
+
+            if (cell->open) {
+                int font_size = 50;
+                const char *text = TextFormat("%d", cell->value);
+                int text_size = MeasureText(text, font_size);
+                int tx = (x + CELL_SIZE/2) - text_size/2;
+                int ty = (y + CELL_SIZE/2) - font_size/2;
+                DrawText(text, tx, ty, font_size, RED);
             }
-        } else if (state == STATE_SHOW_PAIR) {
-            ttime += GetFrameTime();
-            if (ttime > 0.5f) {
-                state = STATE_USER_TURN;
-                picked_cells.first->open = false;
-                picked_cells.second->open = false;
-                picked_cells.first = NULL;
-                picked_cells.second = NULL;
-                ttime = 0.0f;
+
+            if (state == STATE_USER_TURN) {
+                cell_event_handler(cell, is_cell_hovered);
             }
         }
-        
-            ClearBackground(BACKGROUND_COLOR);
-            int sx = GetScreenWidth()/2 - (COLUMNS*CELL_SIZE + (COLUMNS-1)*CELL_GAP)/2;
-            int sy = GetScreenHeight()/2 - (LINES*CELL_SIZE + (LINES-1)*CELL_GAP)/2;
-            
-            for (int line = 0; line < LINES; line++) {
-                for (int column = 0; column < COLUMNS; column++) {
-                    Cell *cell = &field[line][column]; 
-
-                    int x = sx + (column*CELL_SIZE) + (column*CELL_GAP);
-                    int y = sy + (line*CELL_SIZE) + (line*CELL_GAP);
-                    Rectangle cell_rect = {x, y, CELL_SIZE, CELL_SIZE};
-                    bool is_cell_hovered = CheckCollisionPointRec(GetMousePosition(), cell_rect);
-                    
-                    Color color = cell->open ? OPEN_CELL_COLOR : CELL_COLOR; 
-                    DrawRectangle(x, y, CELL_SIZE, CELL_SIZE, color);
-                    
-                    if (cell->open) {
-                        int font_size = 50;
-                        const char *text = TextFormat("%d", cell->value);
-                        int text_size = MeasureText(text, font_size);
-                        int tx = (x + CELL_SIZE/2) - text_size/2;
-                        int ty = (y + CELL_SIZE/2) - font_size/2; 
-                        DrawText(text, tx, ty, font_size, RED);
-                    }
-                    
-                    if (state == STATE_USER_TURN) {
-                        cell_event_handler(cell, is_cell_hovered);
-                    } 
-                }
-            } 
+    }
     elapsed_time += GetFrameTime();
 }
 
