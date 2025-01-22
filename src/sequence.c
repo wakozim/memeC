f5b0974af6d985c2bd5730526dfa5ad9cc2dc3f5
Lookins01 <lookins1q@gmail.com>
2025-01-22 22:33:13 +0500

    [refactor] fixed indentation in `draw_sequence_sreen`

--- src/sequence.c
+++ src/sequence.c
@@ -107,120 +107,120 @@ void draw_bar(void)
 
 void draw_sequence_screen(void)
 {
-            ClearBackground(BACKGROUND_COLOR);
+    ClearBackground(BACKGROUND_COLOR);
 
-            if (IsKeyPressed(KEY_R)) {
-                init_game();
+    if (IsKeyPressed(KEY_R)) {
+        init_game();
+        state = SHOW_SEQUENCE;
+        ttime = 0.0f;
+    }
+
+    if (ttime >= 1.0f) {
+        switch (state) {
+        case SHOW_SEQUENCE: {
+            index += 1;
+            if (index >= sequence_lenght) {
+                index = 0;
+                state = USER_GUESS;
+            }
+            ttime = 0.0f;
+        } break;
+        case USER_GUESS_WRONG: {
+            state = SHOW_SEQUENCE;
+            user_guess_lenght = 0;
+            user_guess_index = -1;
+            ttime = 0.0f;
+        } break;
+        case USER_GUESS_CORRECT: {
+            user_guess_lenght = 0;
+            user_guess_index = -1;
+            if (add_number_to_sequence())
+                state = USER_WIN;
+            else
                 state = SHOW_SEQUENCE;
-                ttime = 0.0f;
+            ttime = 0.0f;
+        } break;
+        default:
+            break;
+        }
+    } else {
+        ttime += GetFrameTime();
+    }
+
+    draw_bar();
+
+    int board_cx = (GetScreenWidth() / 2) - (BOARD_WIDTH / 2);
+    int board_cy = (GetScreenHeight() / 2) - (BAR_HEIGHT + BOARD_HEIGHT / 2);
+    board_cy += BAR_HEIGHT;
+    for (int iy = 0; iy < BOARD_LINES; iy++) {
+        int y_gap = CELL_GAP_SIZE * iy;
+        for (int ix = 0; ix < BOARD_COLUMNS; ix++) {
+            int x_gap = CELL_GAP_SIZE * ix;
+            int cell_index = (iy * BOARD_LINES + ix);
+            Rectangle cell_rect = { board_cx + ix*CELL_SIZE + x_gap, board_cy + iy*CELL_SIZE + y_gap, CELL_SIZE, CELL_SIZE};
+            bool is_hovered = CheckCollisionPointRec(GetMousePosition(), cell_rect);
+
+            Color color = CELL_DEFAULT_COLOR;
+            switch (state) {
+            case SHOW_SEQUENCE: {
+                if (ttime > 0.2f && sequence[index] == cell_index)
+                    color = CELL_SHOWCASE_COLOR;
+            } break;
+            case USER_GUESS:
+            case USER_GUESS_WRONG:
+            case USER_GUESS_CORRECT: {
+                if (user_guess_index == cell_index)
+                    color = CELL_USER_CHOICE_COLOR;
+            } break;
+            default:
+                break;
             }
 
-            if (ttime >= 1.0f) {
-                switch (state) {
-                case SHOW_SEQUENCE: {
-                    index += 1;
-                    if (index >= sequence_lenght) {
-                        index = 0;
-                        state = USER_GUESS;
-                    }
-                    ttime = 0.0f;
-                } break;
-                case USER_GUESS_WRONG: {
-                    state = SHOW_SEQUENCE;
-                    user_guess_lenght = 0;
-                    user_guess_index = -1;
-                    ttime = 0.0f;
-                } break;
-                case USER_GUESS_CORRECT: {
-                    user_guess_lenght = 0;
-                    user_guess_index = -1;
-                    if (add_number_to_sequence())
-                        state = USER_WIN;
-                    else
-                        state = SHOW_SEQUENCE;
-                    ttime = 0.0f;
-                } break;
-                default:
-                    break;
-                }
-            } else {
-                ttime += GetFrameTime();
-            }
+            DrawRectangleRec(cell_rect, color);
 
-            draw_bar();
-
-            int board_cx = (GetScreenWidth() / 2) - (BOARD_WIDTH / 2);
-            int board_cy = (GetScreenHeight() / 2) - (BAR_HEIGHT + BOARD_HEIGHT / 2);
-            board_cy += BAR_HEIGHT;
-            for (int iy = 0; iy < BOARD_LINES; iy++) {
-                int y_gap = CELL_GAP_SIZE * iy;
-                for (int ix = 0; ix < BOARD_COLUMNS; ix++) {
-                    int x_gap = CELL_GAP_SIZE * ix;
-                    int cell_index = (iy * BOARD_LINES + ix);
-                    Rectangle cell_rect = { board_cx + ix*CELL_SIZE + x_gap, board_cy + iy*CELL_SIZE + y_gap, CELL_SIZE, CELL_SIZE};
-                    bool is_hovered = CheckCollisionPointRec(GetMousePosition(), cell_rect);
-
-                    Color color = CELL_DEFAULT_COLOR;
-                    switch (state) {
-                    case SHOW_SEQUENCE: {
-                        if (ttime > 0.2f && sequence[index] == cell_index)
-                            color = CELL_SHOWCASE_COLOR;
-                    } break;
-                    case USER_GUESS:
-                    case USER_GUESS_WRONG:
-                    case USER_GUESS_CORRECT: {
-                        if (user_guess_index == cell_index)
-                            color = CELL_USER_CHOICE_COLOR;
-                    } break;
-                    default:
-                        break;
-                    }
-
-                    DrawRectangleRec(cell_rect, color);
-
-                    if (state == USER_GUESS) {
-                        if (is_hovered && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-                            user_guess_index = cell_index;
-                            if (sequence[user_guess_lenght] == cell_index) {
-                                user_guess_lenght += 1;
-                                if (user_guess_lenght >= sequence_lenght) {
-                                    state = USER_GUESS_CORRECT;
-                                    ttime = 0.0f;
-                                }
-                            } else {
-                                ttime = 0.0f;
-                                state = USER_GUESS_WRONG;
-                            }
+            if (state == USER_GUESS) {
+                if (is_hovered && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                    user_guess_index = cell_index;
+                    if (sequence[user_guess_lenght] == cell_index) {
+                        user_guess_lenght += 1;
+                        if (user_guess_lenght >= sequence_lenght) {
+                            state = USER_GUESS_CORRECT;
+                            ttime = 0.0f;
                         }
+                    } else {
+                        ttime = 0.0f;
+                        state = USER_GUESS_WRONG;
                     }
                 }
             }
+        }
+    }
 
-            if (state == USER_WIN) {
-                int height = 200;
-                int width = 400;
-                int x = GetScreenWidth() / 2 - width / 2;
-                int y = GetScreenHeight() / 2 - height / 2;
-                Color color = { .r = 0x69, .g = 0x69, .b = 0x69, .a = 0xEF};
+    if (state == USER_WIN) {
+        int height = 200;
+        int width = 400;
+        int x = GetScreenWidth() / 2 - width / 2;
+        int y = GetScreenHeight() / 2 - height / 2;
+        Color color = { .r = 0x69, .g = 0x69, .b = 0x69, .a = 0xEF};
 
-                DrawRectangle(x, y, width, height, color);
+        DrawRectangle(x, y, width, height, color);
 
-                const char *restart_text = "Press [R] for restart";
-                int restart_text_size = 25;
-                int restart_text_width = MeasureText(restart_text, restart_text_size);
+        const char *restart_text = "Press [R] for restart";
+        int restart_text_size = 25;
+        int restart_text_width = MeasureText(restart_text, restart_text_size);
 
-                const char *win_text = "You win!";
-                int win_text_size = 69;
-                int win_text_width = MeasureText(win_text, win_text_size);
+        const char *win_text = "You win!";
+        int win_text_size = 69;
+        int win_text_width = MeasureText(win_text, win_text_size);
 
-                int win_text_x = x + width/2 - win_text_width/2;
-                int win_text_y = y + height/2 - (win_text_size + restart_text_size + 10)/2;
-                DrawText(win_text, win_text_x, win_text_y, win_text_size, GREEN);
+        int win_text_x = x + width/2 - win_text_width/2;
+        int win_text_y = y + height/2 - (win_text_size + restart_text_size + 10)/2;
+        DrawText(win_text, win_text_x, win_text_y, win_text_size, GREEN);
 
-                int restart_text_x = x + width/2 - restart_text_width/2;
-                int restart_text_y = win_text_y + 10 + win_text_size;
-                DrawText(restart_text, restart_text_x, restart_text_y, restart_text_size, GREEN);
-            }
+        int restart_text_x = x + width/2 - restart_text_width/2;
+        int restart_text_y = win_text_y + 10 + win_text_size;
+        DrawText(restart_text, restart_text_x, restart_text_y, restart_text_size, GREEN);
+    }
 }
 
 
